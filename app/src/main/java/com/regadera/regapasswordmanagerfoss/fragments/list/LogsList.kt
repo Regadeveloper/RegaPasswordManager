f22f05add3f676324cf34ae702ec4179eb0b808b
raulregadera <raulregadera@gmail.com>
2021-04-18 22:11:41 +0200

    fixed some dependencies issues

--- app/src/main/java/com/regadera/regapasswordmanagerfoss/fragments/list/LogsList.kt
+++ app/src/main/java/com/regadera/regapasswordmanagerfoss/fragments/list/LogsList.kt
@@ -12,8 +12,11 @@ class logsList : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+    
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logs_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_logs_list, container, false)
+
+        return view
     }
 }
